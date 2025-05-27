51bb071e6b7dee21f1d9808cc483b5d7858c7459
Juan Carrasquer <juancarrasquer@gmail.com>
2025-05-27 22:29:03 +0200

    seeder sin aeropuertos 3

--- routes/web.php
+++ routes/web.php
@@ -138,7 +138,7 @@ Route::get('/auth/github/callback', function () {
 Route::get('auth/google', function () {
     return Socialite::driver('google')->with(["prompt" => "select_account"]) ->redirect();
 });
-// magia 2
+// magia 3
 Route::get('auth/google/callback', function () {
 
     $user = Socialite::driver('google')->stateless()->user();
